d69fcae2f0b6fea0a3c935b235a8ea564803150a
driss292 <drisskaci@outlook.fr>
2022-06-29 16:43:42 +0200

    validate-username & mail

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 const inpUtilisateur = document.querySelector(
     ".form-groupe:nth-child(1) input"
 );
-console.log(inpUtilisateur);
+const inpMail = document.querySelector(".form-groupe:nth-child(2) input");
+const inpMdp = document.querySelector(".form-groupe:nth-child(3) input");
+const inpConfirme = document.querySelector(".form-groupe:nth-child(4) input");
+const allImg = document.querySelectorAll(".icone-verif");
+const allSpan = document.querySelectorAll("span");
+const allLigne = document.querySelectorAll(".ligne div");
+
+// Validation Nom d'utilisateur
+inpUtilisateur.addEventListener("input", (e) => {
+    if (e.target.value.length >= 3) {
+        allImg[0].style.display = "inline";
+        allImg[0].src = "ressources/check.svg";
+        allSpan[0].style.display = "none";
+    } else {
+        allImg[0].style.display = "inline";
+        allImg[0].src = "ressources/error.svg";
+        allSpan[0].style.display = "inline";
+    }
+});
+
+// Validation Email
+inpMail.addEventListener("input", (e) => {
+    const regexEmail = /\S+@\S+\.\S+/;
+    if (e.target.value.search(regexEmail) === 0) {
+        allImg[1].style.display = "inline";
+        allImg[1].src = "ressources/check.svg";
+        allSpan[1].style.display = "none";
+    } else if (e.target.value.search(regexEmail) === -1) {
+        allImg[1].style.display = "inline";
+        allImg[1].src = "ressources/error.svg";
+        allSpan[1].style.display = "inline";
+    }
+});
