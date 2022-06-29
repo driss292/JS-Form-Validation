b01fa6cb6416a04e3f229c230b00d3c9675371c6
driss292 <drisskaci@outlook.fr>
2022-06-29 17:25:39 +0200

    better

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JS-Form-Validation
 
-<img src="">
+## REGEX
+
+<img src="https://res.cloudinary.com/dbu3ntrbw/image/upload/v1656516314/Capture_d_e%CC%81cran_2022-06-29_a%CC%80_17.24.58_kfry5h.png">
--- app.js
+++ app.js
@@ -10,27 +10,127 @@ const allLigne = document.querySelectorAll(".ligne div");
 
 // Validation Nom d'utilisateur
 inpUtilisateur.addEventListener("input", (e) => {
-    if (e.target.value.length >= 3) {
-        allImg[0].style.display = "inline";
-        allImg[0].src = "ressources/check.svg";
-        allSpan[0].style.display = "none";
+    if (e.target.value !== "") {
+        if (e.target.value.length >= 3) {
+            allImg[0].style.display = "inline";
+            allImg[0].src = "ressources/check.svg";
+            allSpan[0].style.display = "none";
+        } else {
+            allImg[0].style.display = "inline";
+            allImg[0].src = "ressources/error.svg";
+            allSpan[0].style.display = "inline";
+        }
     } else {
-        allImg[0].style.display = "inline";
-        allImg[0].src = "ressources/error.svg";
-        allSpan[0].style.display = "inline";
+        allSpan[0].style.display = "none";
+        allImg[0].style.display = "none";
     }
 });
 
 // Validation Email
 inpMail.addEventListener("input", (e) => {
     const regexEmail = /\S+@\S+\.\S+/;
-    if (e.target.value.search(regexEmail) === 0) {
-        allImg[1].style.display = "inline";
-        allImg[1].src = "ressources/check.svg";
+    if (e.target.value !== "") {
+        if (e.target.value.search(regexEmail) === 0) {
+            allImg[1].style.display = "inline";
+            allImg[1].src = "ressources/check.svg";
+            allSpan[1].style.display = "none";
+        } else if (e.target.value.search(regexEmail) === -1) {
+            allImg[1].style.display = "inline";
+            allImg[1].src = "ressources/error.svg";
+            allSpan[1].style.display = "inline";
+        }
+    } else {
         allSpan[1].style.display = "none";
-    } else if (e.target.value.search(regexEmail) === -1) {
-        allImg[1].style.display = "inline";
-        allImg[1].src = "ressources/error.svg";
-        allSpan[1].style.display = "inline";
+        allImg[1].style.display = "none";
+    }
+});
+
+// Validation MDP
+let valeurInp;
+const specialCar = /[^a-zA-Z0-9]/;
+const alphabet = /[a-z]/i;
+const chiffres = /[0-9]/;
+
+let objValidation = {
+    symbole: 0,
+    lettre: 0,
+    chiffre: 0,
+};
+
+inpMdp.addEventListener("input", (e) => {
+    valeurInp = e.target.value;
+    if (valeurInp.search(specialCar) !== -1) {
+        objValidation.symbole = 1;
+    }
+    if (valeurInp.search(alphabet) !== -1) {
+        objValidation.lettre = 1;
+    }
+    if (valeurInp.search(chiffres) !== -1) {
+        objValidation.chiffre = 1;
+    }
+
+    if ((e.inputType = "deleteContentBackward")) {
+        if (valeurInp.search(specialCar) === -1) {
+            objValidation.symbole = 0;
+        }
+        if (valeurInp.search(alphabet) === -1) {
+            objValidation.lettre = 0;
+        }
+        if (valeurInp.search(chiffres) === -1) {
+            objValidation.chiffre = 0;
+        }
+    }
+
+    let testAll = 0;
+    for (const property in objValidation) {
+        if (objValidation[property] > 0) {
+            testAll++;
+        }
+    }
+    if (testAll !== 0) {
+        if (testAll < 3) {
+            allSpan[2].style.display = "inline";
+            allImg[2].style.display = "inline";
+            allImg[2].src = "ressources/error.svg";
+        } else {
+            allSpan[2].style.display = "none";
+            allImg[2].src = "ressources/check.svg";
+        }
+    } else {
+        allSpan[2].style.display = "none";
+        allImg[2].style.display = "none";
+    }
+
+    // Force MPD
+    if (valeurInp.length <= 6 && valeurInp.length > 0) {
+        allLigne[0].style.display = "block";
+        allLigne[1].style.display = "none";
+        allLigne[2].style.display = "none";
+    } else if (valeurInp.length > 6 && valeurInp.length <= 9) {
+        allLigne[0].style.display = "block";
+        allLigne[1].style.display = "block";
+        allLigne[2].style.display = "none";
+    } else if (valeurInp.length > 9) {
+        allLigne[0].style.display = "block";
+        allLigne[1].style.display = "block";
+        allLigne[2].style.display = "block";
+    } else if (valeurInp.length === 0) {
+        allLigne[0].style.display = "none";
+        allLigne[1].style.display = "none";
+        allLigne[2].style.display = "none";
+    }
+});
+
+// Confirmation MDP
+
+inpConfirme.addEventListener("input", (e) => {
+    if (e.target.value.length === 0) {
+        allImg[3].style.display = "inline";
+        allImg[3].src = "ressources/error.svg";
+    } else if (e.target.value === valeurInp) {
+        allImg[3].style.display = "inline";
+        allImg[3].src = "ressources/check.svg";
+    } else {
+        allImg[3].src = "ressources/error.svg";
     }
 });
